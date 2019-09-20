a9b51af417581b4c54e3de2d59b89b8183dfcf02
Taylor Sahli <taylorsahli837@gmail.com>
2019-09-20 15:02:36 -0600

    Scraping to CSV file and emailing working

--- FootballScraper/FootballScraper.py
+++ FootballScraper/FootballScraper.py
@@ -2,11 +2,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import bs4
+import yagmail
+import csv
 
-file = open("C:\\Users\\tsahli\\Documents\\selenium\\cbspass.txt", "r")
-lines = file.readlines()
+passFile = open("C:\\Users\\tsahli\\Documents\\selenium\\cbspass.txt", "r")
+lines = passFile.readlines()
 username = lines[0]
 password = lines[1]
+
+emailList = []
+emailFile = open("C:\\Users\\tsahli\\Documents\\selenium\\emails.txt", "r")
+emailLines = emailFile.readlines()
+for line in emailLines:
+    if not None:
+        emailList.append(line)
 
 driver = webdriver.Firefox()
 driver.get("https://www.cbssports.com/login?product_abbrev=opm&xurl=http%3A%2F%2Ftaylorelectricoffice.football.cbssports.com%2Foffice-pool%2Fstandings%2Flive&master_product=26040")
@@ -16,13 +25,26 @@ usernameField.send_keys(username)
 passwordField.send_keys(password)
 driver.find_element_by_name("_submit").click()
 
-currentPage = driver.page_source()
-print(currentPage)
+currentPage = driver.page_source
+soup = bs4.BeautifulSoup(currentPage, "lxml")
+rows = soup.findAll("tr", {'class' : ["bg2","bgFan"]})
 
+picks = []
+for row in rows:
+    thisPick = []
+    cells = row.findChildren("td")
+    for cell in cells:
+        thisPick.append(cell.get_text())
+    picks.append(thisPick)
 
+with open('picks.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(picks)
 
-
-
+emailAttachment = ".\\picks.csv"
+yag = yagmail.SMTP('tspythonprojects@gmail.com')
+contents = ["Attached are this weeks picks.", emailAttachment]
+yag.send(to=emailList, subject='This Weeks NFL Picks', contents=contents)
 
 driver.quit()
 
